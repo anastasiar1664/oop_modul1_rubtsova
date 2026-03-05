a9b7a68cf632942915b8c914188df445011c270b
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-05 14:31:45 +0300

    Added Book

---
+++ src/LibraryApp/Book.cs
@@ -0,0 +1,39 @@
+using System.Text.RegularExpressions;
+
+namespace BookLibraryApp
+{
+    class Book
+    {
+        public string Title;
+        public string Genre;
+        public int Year;
+        public string Author;
+        public bool IsCompleted = false;
+        public Book(string genre, string title, int year, string author, bool iscompleted)
+        {
+            Title = title;
+            Genre = genre;
+            Year = year;
+            Author = author;
+            IsCompleted = iscompleted;
+        }
+        public void ShowInfo()
+        {
+            if (IsCompleted == false)
+            {
+                System.Console.WriteLine($"название книги {Title}, Жанр: {Genre}, Год {Year}, Автор {Author}, Статус : не прочитана.");
+            }
+            else
+            {
+                System.Console.WriteLine($"название книги {Title}, Жанр: {Genre}, Год {Year}, Автор {Author}, Статус: прочитана.");
+            }
+
+
+        }
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+            System.Console.WriteLine($"✓ Книга {Title} отмечена как прочитанная");
+        }
+    }
+}
